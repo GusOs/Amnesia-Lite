2db1223604e94340ef5f7919fd87d8f78fdb3e99
GusOs <gus_92@hotmail.es>
2020-12-10 19:47:38 +0100

    Play radio

---
+++ Amnesia Lite/Assets/Scripts/Radio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+
+    private Collider radioCollision;
+
+    public AudioSource radio;
+
+    private bool RadioPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        radioCollision = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Play if catch object in scene
+    private void OnTriggerEnter(Collider radioCollision)
+    {
+        if(radioCollision.CompareTag("Player"))
+        {
+            radio.Play();
+            RadioPlay = true;
+        }
+    }
+}
